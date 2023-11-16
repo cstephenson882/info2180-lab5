35583bc1773a341e4ca7452004fe537c7baad334
calvin_udemy_python <cstephenson882@gmail.com>
2023-11-16 05:54:54 -0500

    Ex 4: COUNTRIES BEING DISPLAYED IN HTML TABLE.

--- world.php
+++ world.php
@@ -10,12 +10,13 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 if (isset($_GET['country'])) {
     $country = $_GET['country'];
 
-    // Use a prepared statement to prevent SQL injection
+    //Prepared statement to prevent SQL injection
     $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+    //var_dump($stmt);
     $stmt->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
 
     // Output the SQL query for debugging
-    // var_dump($stmt->queryString);
+     //var_dump($stmt->queryString);
 
     $stmt->execute();
 } else {
@@ -27,7 +28,7 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Output the received GET parameters and results for debugging
 // var_dump($_GET);
-// var_dump($results);
+ //var_dump($results);
 
 // Output the HTML list
 ?>
@@ -36,3 +37,27 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
     <?php endforeach; ?>
 </ul>
+<!-- working up to Question 4 -->
+
+<table border="1">
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+          
+        <?php foreach ($results as $row): ?>
+          <tr>
+           <td> <?= $row['name']?></td>
+           <td> <?= $row['continent']?></td>
+           <td> <?= $row['independence_year']?></td>
+           <td> <?= $row['head_of_state']?></td>
+           
+           <!-- '<td>' . $row['continent'] . '</td>';
+           '<td>' . $row['independence_year'] . '</td>';
+           '<td>' . $row['head_of_state'] . '</td>'; -->
+           </tr>
+        <?php endforeach; ?>
+        
+</table>
